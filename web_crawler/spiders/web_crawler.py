e54ebfe1f259f61ff07ff748e59278b9c7862026
Desmond Chi <desmond.chi.g@gmail.com>
2024-05-01 03:59:34 -0700

    Pass command line arguments into `./crawler.sh` Bash script.
    Usage for running the crawler is:
    `./crawler.sh <seed_url_file_name> <num_pages> <hops_away>
    <output_dir_name>`

--- web_crawler/spiders/web_crawler.py
+++ web_crawler/spiders/web_crawler.py
@@ -6,18 +6,24 @@ import os
 
 class WebCrawlerSpider(scrapy.Spider):
     name = "web_crawler"
-    # allowed_domains = ["example.com"]
-    seed_urls = ["https://www.github.com/desmondcchi"]
 
     def __init__(self, *args, **kwargs):
-        # super(WebCrawlerSpider, self).__init__(*args, **kwargs)
-        self.output_dir_name = kwargs.get("OUTPUT_DIR_NAME", "scraped_html_data")
+        self.seed_file_name = kwargs.get("SEED_FILE_NAME", "")
+        self.num_pages = kwargs.get("NUM_PAGES", 0)
+        self.hops_away = kwargs.get("HOPS_AWAY", 0)
+        self.output_dir_name = kwargs.get("OUTPUT_DIR_NAME", "scraped_html_pages")
 
     def start_requests(self) -> Iterable[scrapy.Request]:
-        for url in self.seed_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        seed_urls_file = open(
+            os.path.abspath(
+                os.path.join(os.path.dirname(__file__), f"../../{self.seed_file_name}")
+            ),
+            "r",
+        )
 
-        # return super().start_requests()
+        self.start_urls = [line.strip() for line in seed_urls_file if line.strip()]
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         soup = BeautifulSoup(response.text, "html.parser")
