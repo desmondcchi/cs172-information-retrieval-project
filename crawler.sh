3a45b9c933b675ac2f110527ac7dce90ce44a448
Desmond Chi <desmond.chi.g@gmail.com>
2024-05-02 00:49:00 -0700

    Add threshold for hops-away.
    The parse function keeps track of the current number of hops-away and
    will stop if it surpasses the threshold passed in from the command-line
    argument.

--- crawler.sh
+++ crawler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Usage: ./crawler.sh <seed_file.txt> <num_pages> <hops_away> <output_dir_name>
 
-scrapy crawl web_crawler -a SEED_FILE_NAME=$1 -a NUM_PAGES=$2 -a HOPS_AWAY=$3 -a OUTPUT_DIR_NAME=$4
+scrapy crawl web_crawler -a SEED_FILE_NAME=$1 -a MAX_NUM_PAGES=$2 -a MAX_HOPS_AWAY=$3 -a OUTPUT_DIR_NAME=$4
