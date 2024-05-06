841a2d020babd1e3052229d9f0626d989437b59f
Desmond Chi <desmond.chi.g@gmail.com>
2024-05-06 13:57:38 -0700

    Parse through HTML content to retrieve text content. (#7)
    Before, we were saving the entire HTML file into a folder.

--- crawler.sh
+++ crawler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Usage: ./crawler.sh <seed_file.txt> <num_pages> <hops_away> <output_dir_name>
+# Usage: ./crawler.sh <seed_file.txt> <allowed_domains.txt> <num_pages> <hops_away> <json_name>
 
-scrapy crawl web_crawler -a SEED_FILE_NAME=$1 -a MAX_NUM_PAGES=$2 -a MAX_HOPS_AWAY=$3 -a OUTPUT_DIR_NAME=$4
+scrapy crawl web_crawler -a SEED_FILE_NAME=$1 -a ALLOWED_DOMAINS_FILE_NAME=$2 -a MAX_NUM_PAGES=$3 -a MAX_HOPS_AWAY=$4 -o $5
