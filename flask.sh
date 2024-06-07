21f071c9e58c8be019d246fb79f241e6d04c355a
btruj007 <102835844+btruj007@users.noreply.github.com>
2024-06-06 18:08:11 -0700

    Script Works again

--- flask.sh
+++ flask.sh
@@ -3,6 +3,6 @@
 # Usage:    ./flask.sh
 #           sh flask.sh
 #           bash flask.sh
-
-export FLASK_APP=flask/flask_server
+cd flask/
+export FLASK_APP=flask_server
 flask run -h 0.0.0.0 -p 8888
